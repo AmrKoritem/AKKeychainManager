f30d857756ce9687d30861c1e078d0a407972af7
Amr Koritem <amr.koritem92@gmail.com>
2023-02-14 00:36:42 +0200

    starter code base added

--- Sources/AKKeychainManager/AKKeychainManager.swift
+++ Sources/AKKeychainManager/AKKeychainManager.swift
@@ -6,5 +6,114 @@
 //
 
 import Foundation
+import Security
 
-class AKKeychainManager {}
+let kSecClassValue = NSString(format: kSecClass)
+let kSecAttrAccountValue = NSString(format: kSecAttrAccount)
+let kSecValueDataValue = NSString(format: kSecValueData)
+let kSecClassGenericPasswordValue = NSString(format: kSecClassGenericPassword)
+let kSecAttrServiceValue = NSString(format: kSecAttrService)
+let kSecMatchLimitValue = NSString(format: kSecMatchLimit)
+let kSecReturnDataValue = NSString(format: kSecReturnData)
+let kSecMatchLimitOneValue = NSString(format: kSecMatchLimitOne)
+
+public class AKKeychainManager {
+    static let shared = AKKeychainManager()
+
+    private init() {}
+
+    func update(service: String, account: String, data: String) {
+        guard let dataFromString = data.data(using: String.Encoding.utf8, allowLossyConversion: false) else { return }
+
+        // Instantiate a new default keychain query
+        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, service, account], forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue])
+
+        let status = SecItemUpdate(keychainQuery as CFDictionary, [kSecValueDataValue:dataFromString] as CFDictionary)
+
+        guard status != errSecSuccess,
+              let err = SecCopyErrorMessageString(status, nil) else { return }
+        print("Read failed: \(err)")
+    }
+
+    func remove(service: String, account: String) {
+        // Instantiate a new default keychain query
+        let keychainQuery = NSMutableDictionary(
+            objects: [
+                kSecClassGenericPasswordValue,
+                service,
+                account,
+                kCFBooleanTrue
+            ],
+            forKeys: [
+                kSecClassValue,
+                kSecAttrServiceValue,
+                kSecAttrAccountValue,
+                kSecReturnDataValue
+            ]
+        )
+
+        // Delete any existing items
+        let status = SecItemDelete(keychainQuery as CFDictionary)
+        guard status != errSecSuccess,
+              let err = SecCopyErrorMessageString(status, nil) else { return }
+        print("Remove failed: \(err)")
+    }
+
+    func save(service: String, account: String, data: String) {
+        guard let dataFromString = data.data(using: String.Encoding.utf8, allowLossyConversion: false) else { return }
+        // Instantiate a new default keychain query
+        let keychainQuery: NSMutableDictionary = NSMutableDictionary(
+            objects: [
+                kSecClassGenericPasswordValue,
+                service,
+                account,
+                dataFromString
+            ],
+            forKeys: [
+                kSecClassValue,
+                kSecAttrServiceValue,
+                kSecAttrAccountValue,
+                kSecValueDataValue
+            ]
+        )
+
+        // Add the new keychain item
+        let status = SecItemAdd(keychainQuery as CFDictionary, nil)
+        guard status != errSecSuccess,
+              let err = SecCopyErrorMessageString(status, nil) else { return }
+        print("Write failed: \(err)")
+    }
+
+    func load(service: String, account:String) -> String? {
+        // Instantiate a new default keychain query
+        // Tell the query to return a result
+        // Limit our results to one item
+        let keychainQuery: NSMutableDictionary = NSMutableDictionary(
+            objects: [
+                kSecClassGenericPasswordValue,
+                service,
+                account,
+                kCFBooleanTrue,
+                kSecMatchLimitOneValue
+            ],
+            forKeys: [
+                kSecClassValue,
+                kSecAttrServiceValue,
+                kSecAttrAccountValue,
+                kSecReturnDataValue,
+                kSecMatchLimitValue
+            ]
+        )
+
+        var dataTypeRef :AnyObject?
+        // Search for the keychain items
+        let status: OSStatus = SecItemCopyMatching(keychainQuery, &dataTypeRef)
+
+        guard status == errSecSuccess else {
+            print("Nothing was retrieved from the keychain. Status code \(status)")
+            return nil
+        }
+        guard let retrievedData = dataTypeRef as? Data else { return nil }
+        return String(data: retrievedData, encoding: String.Encoding.utf8)
+    }
+}
