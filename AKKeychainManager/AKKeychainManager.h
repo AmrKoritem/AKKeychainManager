7c16eb3f7da7d5d81508f6fc6b7df31934d236fe
Amr Koritem <amr.koritem92@gmail.com>
2023-02-13 15:50:34 +0200

    Initial Commit

---
+++ AKKeychainManager/AKKeychainManager.h
@@ -0,0 +1,18 @@
+//
+//  AKKeychainManager.h
+//  AKKeychainManager
+//
+//  Created by Amr Koritem on 13/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKKeychainManager.
+FOUNDATION_EXPORT double AKKeychainManagerVersionNumber;
+
+//! Project version string for AKKeychainManager.
+FOUNDATION_EXPORT const unsigned char AKKeychainManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKKeychainManager/PublicHeader.h>
+
+
